e1943d7251ba18b6dc24cf677eec958d0bf335ce
meysam-amini <meysamamini7394@gmail.com>
2024-04-30 17:16:29 +0330

    add swagger

--- src/main/java/com/meysam/csvmanager/config/swagger/SwaggerConfig.java
+++ src/main/java/com/meysam/csvmanager/config/swagger/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package com.meysam.csvmanager.config.swagger;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
 
     @Bean
